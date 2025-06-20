836abb71319073bce5970c7dfd9727916aabde82
istiak.ahmed.enosis <istiak.ahmed.enosis@powerhousedynamics.com>
2025-06-20 16:15:51 +0600

    update find train name script

--- prepare-data/find-train-names.js
+++ prepare-data/find-train-names.js
@@ -1,6 +1,10 @@
-const puppeteer = require("puppeteer");
-const fs = require("fs");
-const path = require("path");
+import puppeteer from "puppeteer";
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -52,11 +56,11 @@ async function writeTrainNamesToFile() {
     2
   )};`;
 
-  const filePath = path.join(__dirname, "trainNames.ts");
+  const filePath = path.join(__dirname, "trainNames.js");
 
   fs.writeFileSync(filePath, resultTsContent);
 
-  console.log("Train names written to trainNames.ts");
+  console.log("Train names written to trainNames.js");
 }
 
 writeTrainNamesToFile();

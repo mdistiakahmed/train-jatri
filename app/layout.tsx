2442ddf39a21b63fcf44cc11d98b82d15c422d7e
istiak.ahmed.enosis <istiak.ahmed.enosis@powerhousedynamics.com>
2025-08-07 15:21:05 +0600

    fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,6 @@ export default function RootLayout({
     <html lang="en">
         <head>
             <script 
-                data-cfasync="false"
                 async 
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9851111861096184"
                 crossOrigin="anonymous">

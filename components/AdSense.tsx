e2687076525df21116ebc6b841e0dc3d1f2d9c2e
istiak.ahmed.enosis <istiak.ahmed.enosis@powerhousedynamics.com>
2025-09-15 23:13:44 +0600

    Update adsense

--- components/AdSense.tsx
+++ components/AdSense.tsx
@@ -11,12 +11,7 @@ export default function AdSense() {
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
       crossOrigin="anonymous"
       data-ad-client="ca-pub-9851111861096184"
-      onLoad={() => {
-        // Initialize AdSense only once
-        if (!window.adsbygoogle || !window.adsbygoogle.loaded) {
-          (window.adsbygoogle = window.adsbygoogle || []).push({});
-        }
-      }}
+      
     />
   );
 }

34a215a6d11e56ffe6a3dd591d4708710f13f46d
Md Istiak Ahmed <istinishat@gmail.com>
2026-02-14 21:50:40 +0700

    fix color for footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   
   return (
-    <footer className="bg-red-900 text-white pt-12 pb-6">
+    <footer className="bg-gray-900 text-white pt-12 pb-6">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Brand Info */}
